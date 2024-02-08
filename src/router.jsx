9941fa7e547d8620cd725935155087119f08840d
PADMAVATHI SJ <padmasj54@gmail.com>
2024-02-08 13:47:14 +0530

    router.jsx file

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <App />
+
+  <Router>  
+    <App/>
   </Router>,
   document.getElementById('root')
+
 );
